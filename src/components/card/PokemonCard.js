4cad3cdee0125960d6938145f9be7b85ce68eaa5
kishor <kishorbhandari608@gmail.com>
2022-01-01 18:19:48 +0530

    fav-icon

--- src/components/card/PokemonCard.js
+++ src/components/card/PokemonCard.js
@@ -40,7 +40,7 @@ export default function PokemonCard(props) {
   const { pokemon, image } = props
   const { id, name } = pokemon
   return (
-    <Grid item xs={12} sm={3} key={id}>
+    <Grid item xs={12} sm={2} key={id}>
       <Link to={"/pokemon/" + id} className={classes.link}>
         <Card className={classes.card}>
           <CardMedia className={classes.cardMedia} image={image}></CardMedia>
